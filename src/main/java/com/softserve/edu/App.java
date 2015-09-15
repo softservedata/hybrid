8820f75d59da5232a87eb700404263569337c75a
petromister10129 <petroandrushchak@gmail.com>
2015-09-15 20:11:54 +0300

    Formating main

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -2,12 +2,9 @@ package com.softserve.edu;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
